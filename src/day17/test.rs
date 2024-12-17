1c85d3e64da2ff66d1a91d7f4272dfe068f54019
Dennis <dennispvandenberg@gmail.com>
2024-12-17 15:47:53 +0100

    Day 17

--- src/day17/test.rs
+++ src/day17/test.rs
@@ -9,7 +9,7 @@ fn test_part1() {
         Register A: 729
         Register B: 0
         Register C: 0
-        
+
         Program: 0,1,5,4,3,0
         "#),
         "4,6,3,5,6,3,5,2,1,0"
@@ -19,7 +19,13 @@ fn test_part1() {
 #[test]
 fn test_part2() {
     assert_eq!(
-        Day17::solve_part2(""),
-        ""
+        Day17::solve_part2(r#"
+        Register A: 2024
+        Register B: 0
+        Register C: 0
+        
+        Program: 0,3,5,4,3,0
+        "#),
+        "117440"
     );
 }
