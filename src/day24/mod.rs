559e9d7bb8e571024e75b8e709b3b4bd703f16e2
Dennis <dennispvandenberg@gmail.com>
2024-12-24 15:54:11 +0100

    Day 24 part 1

--- src/day24/mod.rs
+++ src/day24/mod.rs
@@ -1,11 +1,10 @@
+use crate::solution::Solution;
+use itertools::Itertools;
+use regex::Regex;
 use std::collections::HashMap;
 use std::str::FromStr;
-use itertools::Itertools;
-use nalgebra::vector;
-use regex::Regex;
-use crate::solution::Solution;
-use z3::{ast, Config, Context, Solver};
 use z3::ast::{Ast, Bool};
+use z3::{ast, Config, Context, Solver};
 
 #[cfg(test)]
 mod test;
@@ -15,19 +14,18 @@ pub struct Day24 {}
 enum GateType {
     AND,
     XOR,
-    OR
+    OR,
 }
 
 impl FromStr for GateType {
-
     type Err = ();
 
     fn from_str(input: &str) -> Result<GateType, Self::Err> {
         match input {
-            "AND"  => Ok(GateType::AND),
-            "XOR"  => Ok(GateType::XOR),
-            "OR"  => Ok(GateType::OR),
-            _      => Err(()),
+            "AND" => Ok(GateType::AND),
+            "XOR" => Ok(GateType::XOR),
+            "OR" => Ok(GateType::OR),
+            _ => Err(()),
         }
     }
 }
@@ -37,18 +35,20 @@ struct Gate {
     left: String,
     right: String,
     operation: GateType,
-    output: String
+    output: String,
 }
 
 #[derive(Debug)]
 struct Wiring {
     initial_values: HashMap<String, bool>,
-    connections: Vec<Gate>
+    connections: Vec<Gate>,
 }
 
 impl Wiring {
     fn solve_z(&self) -> u64 {
-        let all_wires = self.connections.iter()
+        let all_wires = self
+            .connections
+            .iter()
             .flat_map(|c| vec![c.left.clone(), c.right.clone(), c.output.clone()])
             .collect::<Vec<_>>();
 
@@ -56,13 +56,14 @@ impl Wiring {
         let ctx = Context::new(&cfg);
         let solver = Solver::new(&ctx);
 
-        let wire_bools = all_wires.iter()
-            .map(|w| (w, Bool::new_const(&ctx, w.clone())))
+        let wire_bools = all_wires
+            .iter()
+            .map(|w| (w.clone(), Bool::new_const(&ctx, w.to_string())))
             .collect::<HashMap<_, _>>();
 
         // Set initial values
         for (wire, value) in &self.initial_values {
-            let bool_var = wire_bools.get(&wire).unwrap();
+            let bool_var = wire_bools.get(&wire.clone()).unwrap();
 
             solver.assert(&bool_var._eq(&Bool::from_bool(&ctx, *value)));
         }
@@ -85,36 +86,49 @@ impl Wiring {
                 }
             }
         }
-        
+
         // Find output
-        let z_wires = wire_bools.keys().filter(|k| k.starts_with("z")).sorted().collect::<Vec<_>>();
-        let z_vars = z_wires.iter().map(|k| wire_bools.get(k.clone()).unwrap()).collect::<Vec<_>>();
+        let z_wires = wire_bools
+            .keys()
+            .filter(|k| k.starts_with("z"))
+            .sorted()
+            .collect::<Vec<_>>();
+        let z_vars = z_wires
+            .iter()
+            .map(|&k| wire_bools.get(k).unwrap())
+            .collect::<Vec<_>>();
 
         // Convert z bools to bitvec
         let mut bitvec = ast::BV::from_u64(&ctx, 0, 1);
-        for (i, b) in z_vars.iter().enumerate() {
+        for (i, b) in z_vars.iter().rev().enumerate() {
             // Turn each Bool into a 1-bit BV: true => 1, else => 0
-            let bit = b.ite(&ast::BV::from_u64(&ctx, 1, 1), &ast::BV::from_u64(&ctx, 0, 1));
+            let bit = b.ite(
+                &ast::BV::from_u64(&ctx, 1, 1),
+                &ast::BV::from_u64(&ctx, 0, 1),
+            );
             if i == 0 {
                 bitvec = bit;
             } else {
-                bitvec = bitvec.concat(&bit); // Concatenate new bit to the left
+                bitvec = bitvec.concat(&bit);
             }
         }
-        
+
         let output = bitvec.to_int(false);
 
         if solver.check() == z3::SatResult::Sat {
             let model = solver.get_model().unwrap();
-            let result = model.eval(&output, true).unwrap();
-            println!("Solved number: {}", result.as_u64().unwrap());
+            let output_result = model.eval(&output, true).unwrap();
+            // for (name, wire_bool) in wire_bools {
+            //     let test = model.eval(&wire_bool, true).unwrap();
+            //     println!("{} = {}", name, test.as_bool().unwrap());
+            // }
+            println!("Solved number: {}", output_result.as_u64().unwrap());
 
-            return result.as_u64().unwrap()
+            output_result.as_u64().unwrap()
         } else {
             println!("Unsat");
+            0
         }
-        
-        0
     }
 }
 
@@ -124,36 +138,39 @@ fn parse_input(input: &str) -> Wiring {
     let processed_input = lines.join("\n");
     let (raw_initial_values, gates) = processed_input.split_once("\n\n").unwrap();
 
-    let initial_values = raw_initial_values.trim().lines()
+    let initial_values = raw_initial_values
+        .trim()
+        .lines()
         .map(|l| l.split_once(":").unwrap())
         .map(|(k, v)| (k.trim().to_string(), v.trim().parse::<u8>().unwrap()))
         .map(|(k, v)| (k, if v > 0 { true } else { false }))
         .collect::<HashMap<String, bool>>();
 
-    let re = Regex::new(r"(.*?) (\w+) (.*?) -> (.*?)").unwrap();
+    let re = Regex::new(r"(.*?) (\w+) (.*?) -> (.*)").unwrap();
 
-    let connections = re.captures_iter(gates.trim())
+    let connections = re
+        .captures_iter(gates.trim())
         .map(|c| c.extract())
-        .map(|(_, [operand1, gate, operand2, output])| Gate {
-            left: operand1.into(),
-            right: operand2.into(),
-            operation: GateType::from_str(gate).unwrap(),
-            output: output.into()
+        .map(|(_, [operand1, gate, operand2, output])| {
+            Gate {
+                left: operand1.into(),
+                right: operand2.into(),
+                operation: GateType::from_str(gate).unwrap(),
+                output: output.into(),
+            }
         })
         .collect::<Vec<Gate>>();
 
     Wiring {
         initial_values,
-        connections
+        connections,
     }
 }
 
 impl Solution for Day24 {
     fn solve_part1(&self, input: &str) -> String {
         let wiring = parse_input(input);
-        
-        println!("{:?}", wiring);
-        
+
         wiring.solve_z().to_string()
     }
 
--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,8 @@ mod solution;
 mod utils;
 
 fn main() {
-    let input = fs::read_to_string(&"data/day23/input.txt").expect("Failed to read file.");
-    let day = day23::Day23 { };
+    let input = fs::read_to_string(&"data/day24/input.txt").expect("Failed to read file.");
+    let day = day24::Day24 { };
     let result = time_snippet!(day.solve(&input));
 
     println!("{}", result)
